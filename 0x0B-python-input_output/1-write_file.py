6b8bad02c5f5073c10d201079653884e28d4602d
Ahmed Magdy <ahmedmagdy92@yahoo.com>
2023-09-13 06:49:10 -0700

    go

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """write_file module.
+
 Contains a function that writes a text file.
 """
+
+
 def write_file(filename="", text=""):
     """
     Writes a string to a text file (UTF8) and
